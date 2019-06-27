61f3526bf6867460c396fd88ef08a4292b2d5160
ugwumadu116 <ugwumadu116@gmail.com>
2019-06-27 12:38:51 +0100

    feature(order): creates user can get all sales order
    - add route to get all sale order
    - add test to cover this endpoint
    
    [Delivers #166870490]

--- test/userOders.test.js
+++ test/userOders.test.js
@@ -114,4 +114,22 @@ describe('Order Endpoint Tests', () => {
     expect(result.body.status).to.eq(400);
     assert.equal(result.body.message, 'You cant update the price of this order');
   });
+  it('GET /order/sale - User GET cars he wants to sale', async () => {
+    const result = await chai
+      .request(app)
+      .get(`${API_PREFIX}/order/sale`)
+      .set('authorization', jwtToken);
+    expect(result).to.have.status(200);
+    expect(result.body.status).to.eq(200);
+    expect(result.body.data).to.be.an('array');
+  });
+  it('GET /order/sale - User GET cars he wants to sale failed User not registered', async () => {
+    const result = await chai
+      .request(app)
+      .get(`${API_PREFIX}/order/sale`)
+      .set('authorization', unregisteredToken);
+    expect(result).to.have.status(400);
+    expect(result.body.status).to.eq(400);
+    assert.equal(result.body.message, 'User not registered');
+  });
 });
