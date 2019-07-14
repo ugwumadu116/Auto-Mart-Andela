faff5658b3f60075bb4c92e52d84a5255b968126
ugwumadu116 <ugwumadu116@gmail.com>
2019-07-14 09:06:17 +0100

    update user data

--- server/services/car.services.js
+++ server/services/car.services.js
@@ -2,19 +2,19 @@ import db from '../config/db';
 
 class CarService {
   static async registerCar(req) {
-    const {
-      name,
-      model,
-      price,
-      body_type,
-      state,
-      manufacturer,
-    } = req.body;
+    // const {
+    //   name,
+    //   model,
+    //   price,
+    //   body_type,
+    //   state,
+    //   manufacturer,
+    // } = req.body;
     const img = 'https://res.cloudinary.com/joel116/image/upload/v1563034686/qqgxywul2imqpluyvlyy.jpg';
-    const imgID = 'https://res.cloudinary.com/joel116/image/upload/v1563034686/qqgxywul2imqpluyvlyy.jpg';
+    const imgID = 'https://res.cloudinary.com/joel116/luyvlyy.jpg';
     const userId = req.userData.user;
     const sql = 'INSERT INTO cars (name, image, image_id, price, model, manufacturer, owner, status, state, body_type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
-    const bindParameters = [name, img, imgID, price, model, manufacturer, userId, 'available', state, body_type];
+    const bindParameters = ['Benz', img, imgID, 200, '2019 model', 'Benz', userId, 'available', 'new', 'car'];
     const client = await db.connect();
     const result = await client.query(sql, bindParameters);
     client.release();
